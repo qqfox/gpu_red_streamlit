3f31eeb6f4a7894d4f0fee63971f413d143f4351
qqfox <trinhquynhfox@gmail.com>
2023-03-26 01:09:49 +0800

    Update streamlitMain.py

--- streamlitMain.py
+++ streamlitMain.py
@@ -23,60 +23,56 @@ features = st.container()
 model = st.container()
 
 
-with header:
-    st.header("PowerColor Web scraping Dashboard")
-    st.text("This is a demo for scrawping the Reddit website. Other website will be developed in April")
-
-with features:
-    st.text("Input crawling information following below example")
-    st.text(""" "6900 xt","6900xt","2022-11-01","2023-03-31" """)
-
-    key_input = st.text_input("Please input the information")
-    
-with model:
-    
-    df = reddit_framework(key_input)
-    
-    # display the dataframe
-    st.text('Sample of data')
-    st.write(df.head())
-    
-    st.download_button(label = "Download Data", data = df.to_csv(),
-                       file_name = "Reddit_dataset.csv",
-                       mime='text/csv')
-
 # with header:
 #     st.header("PowerColor Web scraping Dashboard")
 #     st.text("This is a demo for scrawping the Reddit website. Other website will be developed in April")
-    
-    
-
-# col1, col2 =st.columns(2)
 
 # with features:
-#     with col1:
-#         d1 = st.date_input("Define the start date for crawling.")
-#             #datetime.date(2023,3,1))
-#         start_date = st.write('Start date:', d1)
-#     with col2:
-#         d2 = st.date_input("Define the end date for crawling.")
-#                            #datetime.date(2023,3,1))
-#         end_date = st.write('End date:', d2)
-    
-#     keyword_1 = st.text_input("Please input the name of gpu card: (ex: 6900 xt")
-#     keyword_2 = st.text_input("Please input the name of gpu card: (ex 6900xt")
+#     st.text("Input crawling information following below example")
+#     st.text(""" "6900 xt","6900xt","2022-11-01","2023-03-31" """)
+
+#     key_input = st.text_input("Please input the information")
     
 # with model:
     
-#     df = reddit_framework(keyword_1, keyword_2, start_date, end_date)
+#     df = reddit_framework(key_input)
     
 #     # display the dataframe
-    
+#     st.text('Sample of data')
 #     st.write(df.head())
     
 #     st.download_button(label = "Download Data", data = df.to_csv(),
 #                        file_name = "Reddit_dataset.csv",
 #                        mime='text/csv')
+
+with header:
+    st.header("PowerColor Web scraping Dashboard")
+    st.text("This is a demo for scrawping the Reddit website. Other website will be developed in April")
+    
+    
+
+with features:
+
+    d1 = st.date_input("Define the start date for crawling.")
+    start_date = st.write('Start date:', d1)
+    
+    d2 = st.date_input("Define the end date for crawling.")
+    end_date = st.write('End date:', d2)
+    
+    keyword_1 = st.text_input("Please input the name of gpu card: (ex: 6900 xt")
+    keyword_2 = st.text_input("Please input the name of gpu card: (ex 6900xt")
+    
+with model:
+    
+    df = reddit_framework(keyword_1, keyword_2, start_date, end_date)
+    
+    # display the dataframe
+    
+    st.write(df.head())
+    
+    st.download_button(label = "Download Data", data = df.to_csv(),
+                        file_name = "Reddit_dataset.csv",
+                        mime='text/csv')
     
     
     
