5262d778d90396b101833c333de25bc1663fbbd0
qqfox <trinhquynhfox@gmail.com>
2023-04-05 23:28:48 +0800

    Update streamlitMain_ver2.py

--- streamlitMain_ver2.py
+++ streamlitMain_ver2.py
@@ -30,7 +30,15 @@ with header:
     st.header("PowerColor Web scraping Dashboard")
     st.text("This is a demo for scrawping the Reddit and Techpowerup websites.")
 
-    st.session_state['type'] = st.radio("Choose the website to crawl data",['Reddit','Techpowerup'])
+    def handle_click(new_type):
+        st.session_state.type = new_type
+
+    def handle_click_wo_button():
+        if st.session_state.kind_of_column:
+            st.session_state.type = st.session_state.kind_of_column
+
+    # st.session_state['type'] = st.radio("Choose the website to crawl data",['Reddit','Techpowerup'], on_change = handle_click_wo_button, key = 'kind_of_column')
+    type_of_column = st.radio("Choose the website to crawl data",['Reddit','Techpowerup'], on_change = handle_click_wo_button, key = 'kind_of_column')
 
 # process for reddit
     if st.session_state['type'] == "Reddit":
